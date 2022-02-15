0f087f9a7412c3f2677ba8ca308b7923158fcf2b
Austin <austinpulley19@yahoo.com>
2022-02-15 13:00:28 -0600

    Completed assignment

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = (num1, num2) => 2000 / 100
+    divide (2000,100);
+const square = (num3) =>  2 * 2
+    square (2,2);
+const add = (a, b) => (a + b) 
+    let a = 3;
+    let b = 4;
+    add (a,b);
